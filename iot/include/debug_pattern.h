70c1d095220d55ad6942160f793c147a19b26db3
Fakepng <krit0kasemtewin@gmail.com>
2023-10-13 14:47:53 +0700

    Feat: change booting patten

--- iot/include/debug_pattern.h
+++ iot/include/debug_pattern.h
@@ -5,7 +5,7 @@
 #ifndef debug_pattern_h
 #define debug_pattern_h
 
-#define BOOTING "-.. --- --- .-. -. --- -"
+#define BOOTING "-.. --- -."
 #define FAILED_TO_CONNECT_TO_WIFI "....."
 #define WIFI_CONNECTED "-----"
 #define FAILED_TO_CONNECT_TO_MQTT "....-"
